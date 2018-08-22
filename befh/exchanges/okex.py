32acec7b31020dcf9f14c6483cbffb88897f799a
Yanlei Wu <yanlei.wu@brightridge.intra>
2018-08-22 09:52:50 +0800

    add ping for okex

--- befh/exchanges/okex.py
+++ befh/exchanges/okex.py
@@ -117,6 +117,9 @@ class ExchGwApiOkexWs(WebSocketApiClient):
             trades.append(trade)
         return trades
 
+    def get_ping_msg(self):
+        return '{"event":"ping"}'
+
 
 class ExchGwOkex(ExchangeGateway):
     """
@@ -169,6 +172,8 @@ class ExchGwOkex(ExchangeGateway):
         :param message: Message
         """
         _l = instmt.instmt_code.split('.')
+
+        print(message)
         for item in message:
             if 'channel' in item:
                 # if re.search(r'ok_sub_futureusd_(.*)_depth_(.*)', item['channel']):
